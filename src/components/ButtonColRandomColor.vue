3d63f3fd51e91002e0dd5ad74cd4bb704bdfec84
Laboratorynotices <muenchen.webas@gmail.com>
2022-10-30 10:58:21 +0100

    Обработка ложных срабатываний

--- src/components/ButtonColRandomColor.vue
+++ src/components/ButtonColRandomColor.vue
@@ -1,13 +1,31 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, defineEmits } from 'vue'
+const emit = defineEmits(['touchColorLocked'])
 
 defineProps({
   colorLocked: Boolean
 })
+
+/**
+ * Обрабатываем клик-событие по кнопке.
+ * Нужно, чтобы отсеивать ложные срабатывания при нажатии на клавишу "пробел".
+ * @param {event} e - клик-событие, передающееся от HTML-документа
+ */
+const childTouchColorLocked = (e) => {
+  // Отсеиваем срабатывания, вызванные нажатием клавиши "пробел".
+  if (e.x === 0) {
+    // Если кликать по кнопке мышкой, то с событием передаётся и положение курсора.
+    // А если событие вызывается нажатием клавиши "пробел", то это значение равно нулю.
+    e.preventDefault()
+  } else {
+    // Если правильно сработало, то отправляем событие родительскому компоненту.
+    emit('touchColorLocked')
+  }
+}
 </script>
 
 <template>
-  <button @click="$emit('touchColorLocked')">
+  <button @click="childTouchColorLocked">
     {{ (colorLocked) ? 'locked' : 'unlocked' }}
   </button>
 </template>
